6295c72a18521ef6372c9280115af11ddce74941
KingSherriff <sherriff.kadiri@ucalgary.ca>
2020-03-07 16:37:46 -0700

    Basic completion of the ai still within player class TODO: - Seperate player and AI class - Better organize AI methods - Work on the fortify method again - Better complete the Attack method

--- src/com/Player/Player.java
+++ src/com/Player/Player.java
@@ -1,8 +1,6 @@
 package com.Player;
 import java.util.ArrayList;
 import java.util.Random;
-
-import com.*;
 import com.Board.Board;
 import com.Board.Map.Continent;
 import com.Board.Map.Country;
@@ -18,8 +16,6 @@ public class Player {
 	public Player(int playerNumber, boolean isai, Board b) {
 		this.playerNumber = playerNumber;
 		this.isAI = isai;
-		System.out.println(isAI);
-		System.out.println(playerNumber);
 		board = new Board(b);
 		setAlliance();
 	}
@@ -44,13 +40,21 @@ public class Player {
 	}
 
 
-	
+
 	public boolean equals(Player p) {
 		return this.getPlayerNumber() == p.getPlayerNumber();
 	}
-	
+
 	public int getPlayerNumber() {
 		return playerNumber;
+	}
+	
+	public void setPlayerNumber(int i) {
+		this.playerNumber = i;
+	}
+
+	public String getPlayerName() {
+		return playerName;
 	}
 	
 	public void setPlayerName(String name) {
@@ -62,55 +66,96 @@ public class Player {
 			playerName = name;
 		}
 	}
-	
-	public String getPlayerName() {
-		return playerName;
-	}
-	
+
 	public String getPlayerName(int PlayerNumber) {
 		return playerName;
 	}
-	
+
 	public boolean getIsAI() {
 		return isAI;
 	}
-	
+
 	public Country getCountryToAddTroops(Board b, ArrayList<Country> c) {
 		ArrayList<Continent> continents = b.getContinents();
-		ArrayList<Country> mostTroops = new ArrayList<Country>();
 		int num1 = 0;
 		Random random = new Random();
 		Continent contWithMostTroops = continents.get(0);
-		
+		Country temp = contWithMostTroops.getCountries().get(random.nextInt(contWithMostTroops.getCountries().size()));
+		while(temp.getPlayerOwnerOfCountry() != this.playerNumber) {
+			for(Continent cont : continents) {
+				int contTroopNum = 0;
+				for(Country country : cont.getCountries()) {
+					if(country.getPlayerOwnerOfCountry() == this.playerNumber) {
+						contTroopNum = contTroopNum + country.getNumTroops();
+					}
+					if(contTroopNum > num1) {
+						contWithMostTroops = cont;
+						num1 = contTroopNum;
+					}
+				}
+				temp = contWithMostTroops.getCountries().get(random.nextInt(contWithMostTroops.getCountries().size()));
+			}
+		}
+		return temp;
+	}
+
+	public int aiDraft(int bonusTroops, Board b) {
+		return 1;
+	}
+
+	public boolean getAttackChoice(Board b, ArrayList<Country> c) {
+		ArrayList<Continent> continents = b.getContinents();
+
 		for(Continent cont : continents) {
-			int contTroopNum = 0;
 			for(Country country : cont.getCountries()) {
 				if(country.getPlayerOwnerOfCountry() == this.playerNumber) {
-					contTroopNum = contTroopNum + country.getNumTroops();
-				}
-				if(contTroopNum > num1) {
-					contWithMostTroops = cont;
-					num1 = contTroopNum;
+					ArrayList<Country> borders = country.getBorders();
+					for(Country bord : borders) {
+						if(country.getNumTroops() > bord.getNumTroops() + bord.getNumTroops() * 0.25) {
+							return true;
+						}
+					}
 				}
 			}
 		}
-		return contWithMostTroops.getCountries().get(random.nextInt(contWithMostTroops.getCountries().size()));//.getName();
-	}
-	
-	public int getAiNumberOfTroopsToAdd() {
-		return 0;
-	}
-	
-	public int aiDraft(int bonusTroops, Board b) {
-		if(isAI == true) {
-			
-		}
-		return 3;
-	}
-	
-	
-	public void setPlayerNumber(int i) {
-		this.playerNumber = i;
+		return false;
 	}
 
+	public Country getCountryToAttackFrom(Board b, ArrayList<Country> c) {
+		ArrayList<Continent> continents = b.getContinents();
+
+		for(Continent cont : continents) {
+			for(Country country : cont.getCountries()) {
+				if(country.getPlayerOwnerOfCountry() == this.playerNumber) {
+					ArrayList<Country> borders = country.getBorders();
+					for(Country bord : borders) {
+						if(country.getNumTroops() > bord.getNumTroops() + bord.getNumTroops() * 0.25) {
+							System.out.println(country.getName());
+							return country;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public Country getCountryToAttack(Board b, ArrayList<Country> c) {
+		ArrayList<Continent> continents = b.getContinents();
+
+		for(Continent cont : continents) {
+			for(Country country : cont.getCountries()) {
+				if(country.getPlayerOwnerOfCountry() == this.playerNumber) {
+					ArrayList<Country> borders = country.getBorders();
+					for(Country bord : borders) {
+						if(country.getNumTroops() > bord.getNumTroops() + bord.getNumTroops() * 0.25) {
+							System.out.println(bord.getName());
+							return bord;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
 }
