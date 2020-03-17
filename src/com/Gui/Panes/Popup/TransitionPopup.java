03f7348a933f0744297cc8e3d243842a468be85b
TheAlexTanasescu <alskiboy@yahoo.com>
2020-03-17 00:52:56 -0600

    grabbing

---
+++ src/com/Gui/Panes/Popup/TransitionPopup.java
@@ -0,0 +1,15 @@
+package com.Gui.Panes.Popup;
+
+import com.Board.Board;
+import javafx.animation.Transition;
+
+public class TransitionPopup extends Stage{
+	
+	private Board board;
+	
+
+	public TransitionPopup() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
