37969975ef099c34d7acf000594be44bbac8842a
surya kusjanto <skusjanto@outlook.com>
2020-03-16 19:32:43 -0600

    Added mapblocker in interactive Pane and an Attack Pop-up

--- src/com/Gui/Panes/Popup/AttackPopup.java
+++ src/com/Gui/Panes/Popup/AttackPopup.java
@@ -1,16 +1,56 @@
 package com.Gui.Panes.Popup;
 
+
 import com.Board.Board;
 import com.Board.Map.Country;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.layout.HBox;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+/**
+ * A Pop-up which only contains a single button for attacking.
+ * Upon clicking the attack button, the player will attack the 
+ * selected Country.
+ * @author skusj
+ *
+ */
 public class AttackPopup extends Stage {
+	ImageView attackImg;
+	Board board;
+	Button attack;
 	
-	public AttackPopup() {
+	public AttackPopup(Board board) {
+		this.board = board;
+		Image image = new Image("sword.png"); 
+		attackImg = new ImageView(image);
+		
+		attack = new Button("");
+		attack.setGraphic(attackImg);
+		Scene scene = new Scene(attack);
+		this.setScene(scene);
+		this.setOpacity(0.9);
+		this.initStyle(StageStyle.UNDECORATED);
+		onButtonClick();
+		
+	}
+	
+	public void onButtonClick() {
+		attack.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				board.mapController.clear();
+				board.getInteractivePane().removeMapBlocker();
+				close();
+			}
+
+		});
 		
 	}
 }
