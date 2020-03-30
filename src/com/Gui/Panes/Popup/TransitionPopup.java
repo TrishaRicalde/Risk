d13200be23a0700667c012afd3da54a7bc5d1c7d
Alex Tanasescu <alextanasescu@Dis-One.local>
2020-03-29 22:42:49 -0600

    Grabbing

--- src/com/Gui/Panes/Popup/TransitionPopup.java
+++ src/com/Gui/Panes/Popup/TransitionPopup.java
@@ -5,51 +5,69 @@ import com.Board.Phase;
 
 import javafx.animation.ScaleTransition;
 import javafx.animation.Transition;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class TransitionPopup extends Stage{
-	
+	private StackPane pane;
 	private Board board;
 	Label playerLabel = new Label();
 	Label phaseLabel = new Label();
+	Popup animationPopUp = new Popup();
 	
 
 	public TransitionPopup(Board board) 
 	{
+		pane = new StackPane();
 		this.board = board;	
+		
+		Scene scene = new Scene(pane, 300, 100);
+		this.setScene(scene);
+		/*
+		animationPopUp.getContent().add(pane);
+		animationPopUp.show(this);
+		
+		*/
 	}
 	
 	public void nextTurnTransition(String playerName)
 	{
-		playerLabel.setText(playerName);
-		/*
+		pane.getChildren().clear();
+		playerLabel = new Label(playerName);
 		ScaleTransition tt = new ScaleTransition(Duration.millis(2000), playerLabel);
 		tt.setByX(1.5);
 		tt.setByY(1.5);
 		tt.setCycleCount(1);
 		tt.setAutoReverse(true);
-		*/
-		board.getInteractivePane().getChildren().add(playerLabel);
+	
 		
-		//tt.play();
+		tt.play();
+		pane.getChildren().add(playerLabel);
+		
+		
 		
 		
 	}
 	
 	public void nextPhaseTransition(Phase phase)
 	{
-		phaseLabel.setText(phase.toString());
-		/*ScaleTransition pt = new ScaleTransition(Duration.millis(2000), phaseLabel);
-		pt.setByX(1.5);
-		pt.setByY(1.5);
+		pane.getChildren().clear();
+		phaseLabel = new Label(phase.toString());
+		ScaleTransition pt = new ScaleTransition(Duration.millis(2000), phaseLabel);
+		pt.setByX(5);
+		pt.setByY(5);
 		pt.setCycleCount(1);
 		pt.setAutoReverse(true);
 		
 		pt.play();
-		*/
-		board.getInteractivePane().getChildren().add(phaseLabel);
+		
+		pane.getChildren().add(phaseLabel);
+		
 	}
 
 
