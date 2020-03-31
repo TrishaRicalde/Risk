cea283c33c08cfef5e9d97a94c473aa31bef8ae6
Ramez <ramezhalasah@gmail.com>
2020-03-30 22:47:23 -0600

    Ramez Commit, partially added labellayers for currentNumTroops thingie

--- src/com/Gui/Panes/LabelLayer.java
+++ src/com/Gui/Panes/LabelLayer.java
@@ -1,21 +1,70 @@
 package com.Gui.Panes;
 
+import java.util.ArrayList;
+
 import com.Board.Board;
+import com.Board.Map.Continent;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 public class LabelLayer extends Pane {
+	
+	
+	private ArrayList<Label> AllLabels;
 
 	public LabelLayer(Board board) {
+		AllLabels = new ArrayList<Label>();
+		init();
 		this.setWidth(board.getWidth());
 		this.setHeight(board.getHeight());
-		
-		Label l = new Label("Troop");
-		l.setText("Troop");
-		l.setTranslateX(100.0);
-		l.setTranslateY(100.0);
-		
-		this.getChildren().add(l);
+		for(Label x:AllLabels) {
+		this.getChildren().add(x);
+		}
 	}
+
+	
+	
+	
+	public void init() {
+		
+		// NORTH AMERICA
+			// ALASKA
+			Label alaska = new Label("0");
+			alaska.setTranslateX(63.0);
+			alaska.setTranslateY(61.0);
+			AllLabels.add(alaska);
+			// ALBERTA
+			Label alberta = new Label("1");
+			alberta.setTranslateX(115.0);
+			alberta.setTranslateY(90);
+			AllLabels.add(alberta);
+			// ONTARIO
+			Label ontario = new Label("2");
+			ontario.setTranslateX(169);
+			ontario.setTranslateY(93);
+			AllLabels.add(ontario);
+			// QUEBEC
+			Label quebec = new Label("3");
+			quebec.setTranslateX(226);
+			quebec.setTranslateY(93);
+			AllLabels.add(quebec);
+			// CENTRAL AMERICA
+			Label centralAmerica = new Label("3");
+			centralAmerica.setTranslateX(104);
+			centralAmerica.setTranslateY(190);
+			AllLabels.add(centralAmerica);
+			// WESTERN UNITED STATES
+			Label westernunitedstates = new Label("3");
+			westernunitedstates.setTranslateX(98);
+			westernunitedstates.setTranslateY(132);
+			AllLabels.add(westernunitedstates);
+			// EASTERN UNITED STATES
+			Label easternUnitedStates = new Label("3");
+			easternUnitedStates.setTranslateX(165);
+			easternUnitedStates.setTranslateY(148);
+			AllLabels.add(easternUnitedStates);
+			
+	}
+
 }
