f1bac722437a6e28534983fc5e2e0b834619a2ed
TheAlexTanasescu <alskiboy@yahoo.com>
2020-03-30 19:45:36 -0600

    grabbing

--- src/com/Gui/Panes/Popup/TransitionPopup.java
+++ src/com/Gui/Panes/Popup/TransitionPopup.java
@@ -1,15 +1,20 @@
 package com.Gui.Panes.Popup;
 
 import com.Board.Board;
+
 import com.Board.Phase;
 
 import javafx.animation.ScaleTransition;
 import javafx.animation.Transition;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Popup;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -18,21 +23,34 @@ public class TransitionPopup extends Stage{
 	private Board board;
 	Label playerLabel = new Label();
 	Label phaseLabel = new Label();
-	Popup animationPopUp = new Popup();
+	//Button next;
+	//VBox vbox;
+	//Popup animationPopUp = new Popup();
 	
 
 	public TransitionPopup(Board board) 
 	{
+		/*
+		vbox = new VBox();
+		next = new Button();
+		
+		next.setText("Next");
+		*/
+		
 		pane = new StackPane();
+		
 		this.board = board;	
 		
-		Scene scene = new Scene(pane, 300, 100);
+		Scene scene = new Scene(pane, 720, 240);
 		this.setScene(scene);
-		/*
-		animationPopUp.getContent().add(pane);
-		animationPopUp.show(this);
 		
-		*/
+	/*	
+		vbox.getChildren().add(pane);
+		vbox.getChildren().add(next);
+		vbox.setMargin(pane, new Insets(100, 0, 0, 0));
+		vbox.setMargin(next, new Insets(50, 0, 0, 600));
+	*/
+
 	}
 	
 	public void nextTurnTransition(String playerName)
@@ -63,11 +81,30 @@ public class TransitionPopup extends Stage{
 		pt.setByY(5);
 		pt.setCycleCount(1);
 		pt.setAutoReverse(true);
-		
-		pt.play();
-		
 		pane.getChildren().add(phaseLabel);
+		pt.play();		
+		/*
+		next.setOnAction(new EventHandler<ActionEvent>()
+		{
+				@Override
+				public void handle(ActionEvent event) 
+				{
+					vbox.setVisible(false);
+				}
 		
+		});
+		
+	
+		pt.setOnFinished(new EventHandler<ActionEvent>()
+		{
+				@Override
+				public void handle(ActionEvent event) 
+				{
+					pane.setVisible(false);
+				}
+		
+		});
+	*/	
 	}
 
 
